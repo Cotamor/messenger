4e56813053abd9870ea2f9bbc33e574525a0ae1f
Cotamor <cotamor@gmail.com>
2023-08-12 00:33:53 +0900

    authform ui

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -3,21 +3,23 @@
 import { useForm, FieldValues, SubmitHandler } from 'react-hook-form'
 import { useRouter } from 'next/navigation'
 import { useCallback, useState } from 'react'
+import Input from '@/components/inputs/Input'
+import Button from '@/components/Button'
 
 type Variant = 'LOGIN' | 'REGISTER'
 
 const AuthForm = () => {
   const router = useRouter()
   const [variant, setVariant] = useState<Variant>('LOGIN')
-  const [isLoading,setIsLoading] = useState(false)
+  const [isLoading, setIsLoading] = useState(false)
 
-  const toggleVariant = useCallback(()=>{
-    if(variant === 'LOGIN'){
+  const toggleVariant = useCallback(() => {
+    if (variant === 'LOGIN') {
       setVariant('REGISTER')
     } else {
       setVariant('LOGIN')
     }
-  },[variant])
+  }, [variant])
 
   const {
     register,
@@ -33,16 +35,58 @@ const AuthForm = () => {
 
   const onSubmit: SubmitHandler<FieldValues> = (data) => {
     // TODO login and register functionality
+    setIsLoading(true)
+
+    if (variant === 'REGISTER') {
+      // Axios Register
+    }
+
+    if (variant === 'LOGIN') {
+      // Axios Login
+    }
   }
 
-  const socialAction = () => {}
+  const socialAction = (action: string) => {
+    setIsLoading(true)
+
+    // Nextauth social sign in
+  }
 
   return (
     <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
       <div className="bg-white px-4 py-8 shadow sm:rounded-lg sm:px-10">
         <form className="space-y-6" onSubmit={handleSubmit(onSubmit)}>
-         
-
+          <Input
+            disabled={isLoading}
+            register={register}
+            errors={errors}
+            required
+            id="name"
+            label="Name"
+          />
+          <Input
+            disabled={isLoading}
+            register={register}
+            errors={errors}
+            required
+            id="email"
+            label="Email address"
+            type="email"
+          />
+          <Input
+            disabled={isLoading}
+            register={register}
+            errors={errors}
+            required
+            id="password"
+            label="Password"
+            type="password"
+          />
+          <div className="">
+            <Button disabled={isLoading} fullWidth type='submit'>
+              {variant === 'LOGIN' ? 'Sign in' : 'Register'}
+            </Button>
+          </div>
         </form>
       </div>
     </div>
--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -4,7 +4,7 @@ import AuthForm from "./components/AuthForm";
 export default function Home() {
   return (
     <div className="min-h-full flex flex-col justify-center py-12 sm:px-6 lg:px-8 bg-gray-100">
-      <div className="sm:mx-auto sm:w-full sm:max-w-md bg-pink-200">
+      <div className="sm:mx-auto sm:w-full sm:max-w-md ">
         <Image
           alt="Logo"
           src='/images/logo.png'
